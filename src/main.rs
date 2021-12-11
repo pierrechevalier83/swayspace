bd808e5781a9fb5537f1880712470302ae673e76
Pierre Viseu Chevalier <pierrechevalier83@gmail.com>
2021-12-11 13:06:32 +0000

    Add support for navigation between multiple monitors

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,55 @@
 #![feature(iter_partition_in_place)]
 
+use clap::arg_enum;
 use std::str::FromStr;
 use structopt::StructOpt;
 use swayipc::Connection;
 
-#[derive(Debug)]
+arg_enum! {
+    #[derive(Debug)]
 enum To {
+    Workspace,
+    Output,
+}
+}
+
+arg_enum! {
+    #[derive(Debug)]
+enum Direction {
     Prev,
     Next,
 }
+}
 
-impl FromStr for To {
+#[derive(Debug)]
+enum Do {
+    MoveFocusTo,
+    MoveContainerTo,
+}
+
+impl FromStr for Do {
     type Err = String;
     fn from_str(s: &str) -> Result<Self, Self::Err> {
         match s {
-            "prev-workspace" => Ok(Self::Prev),
-            "next-workspace" => Ok(Self::Next),
-            s => Err(format!("Failed to parse {} as --to. Expected one of [prev-workspace, next-workspace]", s))
+            "move-focus-to" => Ok(Self::MoveFocusTo),
+            "move-container-to" => Ok(Self::MoveContainerTo),
+            _ => Err(format!(
+                "Failed to parse {} as --do. Expected one of [move-focus-to, move-container-to]",
+                s
+            )),
         }
     }
 }
 
-impl std::fmt::Display for To {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        match self {
-            To::Next => write!(f, "next"),
-            To::Prev => write!(f, "prev"),
-        }
-    }
+#[derive(Debug, StructOpt)]
+#[structopt(about = "Automatically create workspaces under sway like gnome does")]
+struct Opt {
+    #[structopt(default_value = "move-focus-to")]
+    command: Do,
+    #[structopt(default_value = "workspace", possible_values = &To::variants(), case_insensitive = true)]
+    to: To,
+    #[structopt(default_value = "next", possible_values = &Direction::variants(), case_insensitive = true)]
+    dir: Direction,
 }
 
 struct WindowManagerState {
@@ -37,6 +59,14 @@ struct WindowManagerState {
     max_workspace_on_focused_output: i32,
     min_workspace_on_focused_output: i32,
     is_current_workspace_empty: bool,
+    // For each output in order of its x position, the num of its visible workspace
+    visible_workspace_per_output: Vec<i32>,
+}
+
+#[derive(PartialEq, Eq, Ord, PartialOrd)]
+struct Output {
+    x_pos: i64,
+    name: String,
 }
 
 impl WindowManagerState {
@@ -52,7 +82,33 @@ impl WindowManagerState {
             .name
             .unwrap();
 
+        let mut outputs = wm
+            .get_outputs()
+            .unwrap()
+            .iter()
+            .map(|o| Output {
+                x_pos: o.rect.x,
+                name: o.name.clone(),
+            })
+            .collect::<Vec<_>>();
+
+        outputs.sort();
+
         let mut all_workspaces = wm.get_workspaces().unwrap();
+        let visible_workspaces = all_workspaces
+            .iter()
+            .filter(|w| w.visible)
+            .collect::<Vec<_>>();
+        let visible_workspace_per_output = outputs
+            .iter()
+            .filter_map(|o| {
+                visible_workspaces
+                    .iter()
+                    .find(|w| w.output == o.name)
+                    .map(|w| w.num)
+            })
+            .collect();
+
         let current_workspace = all_workspaces.iter().find(|w| w.focused).unwrap();
         let is_current_workspace_empty = current_workspace.representation == "";
         let current_workspace = current_workspace.num;
@@ -76,6 +132,7 @@ impl WindowManagerState {
             max_workspace_on_focused_output,
             min_workspace_on_focused_output,
             is_current_workspace_empty,
+            visible_workspace_per_output,
         }
     }
     fn make_new_workspace_at_end(&self) -> i32 {
@@ -123,40 +180,46 @@ impl WindowManagerState {
                 .unwrap_or(self.make_new_workspace_at_end())
         }
     }
+
+    fn visible_workspace_on_next_output(&self) -> i32 {
+        let current_index = self
+            .visible_workspace_per_output
+            .iter()
+            .position(|&x| x == self.current_workspace);
+        current_index
+            .map(|i| {
+                self.visible_workspace_per_output
+                    .iter()
+                    .cycle()
+                    .nth(i + 1)
+                    .unwrap()
+            })
+            .copied()
+            .unwrap_or(self.current_workspace)
+    }
+    fn visible_workspace_on_prev_output(&self) -> i32 {
+        let current_index = self
+            .visible_workspace_per_output
+            .iter()
+            .position(|&x| x == self.current_workspace)
+            .unwrap();
+        let prev_index = if current_index == 0 {
+            self.visible_workspace_per_output.len() - 1
+        } else {
+            current_index - 1
+        };
+        self.visible_workspace_per_output[prev_index]
+    }
 }
 
 fn pick_destination(wm: &mut Connection, opt: Opt) -> i32 {
     let wm_state = WindowManagerState::from_wm(wm);
-    match opt.to {
-        To::Next => wm_state.next_workspace_on_focused_output(),
-        To::Prev => wm_state.prev_workspace_on_focused_output(),
+    match (opt.to, opt.dir) {
+        (To::Workspace, Direction::Next) => wm_state.next_workspace_on_focused_output(),
+        (To::Workspace, Direction::Prev) => wm_state.prev_workspace_on_focused_output(),
+        (To::Output, Direction::Next) => wm_state.visible_workspace_on_next_output(),
+        (To::Output, Direction::Prev) => wm_state.visible_workspace_on_prev_output(),
     }
-}
-
-#[derive(Debug)]
-enum Do {
-    MoveFocus,
-    MoveContainer,
-}
-
-impl FromStr for Do {
-    type Err = String;
-    fn from_str(s: &str) -> Result<Self, Self::Err> {
-        match s {
-            "move-focus" => Ok(Self::MoveFocus),
-            "move-container" => Ok(Self::MoveContainer),
-            _ => Err(format!("Failed to parse {} as --do. Expected one of [move-focus, move-container]", s)),
-        }
-    }
-}
-
-#[derive(Debug, StructOpt)]
-#[structopt(about = "Automatically create workspaces under sway like gnome does")]
-struct Opt {
-    #[structopt(long="do", default_value = "move-focus")]
-    command: Do,
-    #[structopt(long="to", default_value = "next-workspace")]
-    to: To,
 }
 
 fn main() {
@@ -164,15 +227,18 @@ fn main() {
     let opt = Opt::from_args();
     let mut wm = swayipc::Connection::new().unwrap();
     match opt.command {
-        Do::MoveFocus => {
+        Do::MoveFocusTo => {
             let destination = pick_destination(&mut wm, opt);
             wm.run_command(format!("workspace number {}", destination))
                 .unwrap();
         }
-        Do::MoveContainer => {
+        Do::MoveContainerTo => {
             let destination = pick_destination(&mut wm, opt);
-            wm.run_command(format!("move container to workspace number {}", destination))
-                .unwrap();
+            wm.run_command(format!(
+                "move container to workspace number {}",
+                destination
+            ))
+            .unwrap();
             wm.run_command(format!("workspace number {}", destination))
                 .unwrap();
         }
